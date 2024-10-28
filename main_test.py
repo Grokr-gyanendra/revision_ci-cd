94a7a11db454e4aa7a05d4e3e3975691848d0dea
Grokr-gyanendra <gyanendra.shukla@datagrokr.com>
2024-10-28 11:37:57 +0530

    update main, test_main.py

--- main_test.py
+++ main_test.py
@@ -31,14 +31,14 @@ def test_add(client):
     res = client.get('/add', query_string={'x':3, 'y':5})
     # json_data = res.get_json()
     # assert json_data['ans'] == 8
-    assert res.data == 8
+    assert res.data == b'8'
     assert res.status_code == 200
     
 def test_sub(client):
     res = client.get('/sub', query_string={'x':10, 'y':5})
     # json_data = res.get_json()
     # assert json_data['ans'] == 5
-    assert res.data == 5
+    assert res.data == b'5'
     assert res.status_code == 200
     
 def test_sub_invalid(client):
